98a3fd2295b0ce0d143b9b1bf6e7ba7ada511432
Administrator <nepal3491@gmail.com>
2021-02-23 11:38:34 +0545

    question 2

---
+++ lab prpject/question 2.py
@@ -0,0 +1,6 @@
+#write a program that reads the length of the base and the height of the right angle trangle and print the area
+#area. every number is given separated line
+base=int(input("enter the value base"))
+height=int(input("enter the value of height"))
+area= base*height/2
+print(f"the area of right angle traingle is {area}")
