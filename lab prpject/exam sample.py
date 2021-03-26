6171afa6d296c215ca6688081fab44397a558e3c
Administrator <nepal3491@gmail.com>
2021-03-26 13:40:22 +0545

    Exam sample

---
+++ lab prpject/exam sample.py
@@ -0,0 +1,19 @@
+#code to calculate multilication of 3
+num=3
+for i in range(1,11):
+    k=(num * i)
+    print(f"{num}* {i} ={k}")
+print("*************************************************************************")
+
+j=0
+while j<5:
+    if i==3:
+        break
+    print(j)
+    j+=1
+print("*******************************************")
+
+for l in range(1,7):
+    if i==3:
+        continue
+
