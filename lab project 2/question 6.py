067a0c20e5ce91bc983d23e434f680eebbcde3ca
Administrator <nepal3491@gmail.com>
2021-03-30 12:21:48 +0545

    Class work

---
+++ lab project 2/question 6.py
@@ -0,0 +1,16 @@
+#inpit the weight of a person in either kg or lbs.
+#if the person provides his/her weight in kg then convert it into lbs else convert it to kg.
+
+weight=(float(input("enter your weight")))
+weight1=(input("is it in lbs of kg?"))
+if weight1=="kg":
+    convert=weight*2.21
+
+    print(f"your weight in lbs= {convert}")
+elif weight1=="lbs":
+    convert=weight/2.21
+    print(f'your weight in kg is{convert}')
+else:
+    print('i dont understand')
+
+
