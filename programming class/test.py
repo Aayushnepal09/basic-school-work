92bd9f0e0503e0384a96aec56b0c216bd70dcd2e
Administrator <nepal3491@gmail.com>
2021-04-27 11:54:02 +0545

    slide

---
+++ programming class/test.py
@@ -0,0 +1,19 @@
+from tkinter import*
+
+
+root=Tk()
+root.title ('Dialog Box')
+
+
+vertical=Scale(root,from_=0,to=200)
+vertical.pack()
+
+horizontal=Scale(root,from_=0,to=200,orient=HORIZONTAL)
+horizontal.pack()
+
+def slide():
+    my_label=Label(root,text=horizontal.get()).pack()
+    root.geometry(str(horizontal.get))+"x"+str(vertical.get())
+
+my_btn=Button(root,text="click me",command=slide).pack()
+root.mainloop()
