b021641b10b5c907cf680960d6e8f5221abb175c
Administrator <nepal3491@gmail.com>
2021-05-04 11:55:19 +0545

    demo

--- programming class/Class Turtle.py
+++ programming class/Class Turtle.py
@@ -1,14 +1,15 @@
-#importing turtle
-'''import turtle
-screen=turtle.Screen()'''
+# importing turtle
+"""import turtle
+screen=turtle.Screen()"""
 '''or'''
 '''from turtle import *
 screen=Screen()
 '''
 
-from turtle import*
-screen=Screen()
-pen=Turtle()
+from turtle import *
+
+screen = Screen()
+pen = Turtle()
 pen.speed(2)
 pen.begin_fill()
 pen.shape('turtle')
@@ -19,9 +20,9 @@ pen.color('blue')
 pen.left(25)
 pen.backward(200)
 pen.right(50)'''
-for i in range(5):     # star
+for i in range(5):  # star
     pen.forward(300)
     pen.left(144)
 pen.end_fill()
-#done
+# done
 screen.exitonclick()
--- project/database.py
+++ project/database.py
@@ -1,30 +1,30 @@
 import sqlite3
-from tkinter import*
-root=Tk()
+from tkinter import *
+
+root = Tk()
 root.title('database address book')
 
+# database
 
-#database
+# creating a database or connect to one
+conn = sqlite3.connect('addresses_book.db')
 
-#creaing a database or connect to one
-conn=sqlite3.connect(('aaddress_book.db'))
+# creating cursor
+'''cursor is an instance using which you can invoke methods that
+execute SQLITE statement, fetch data from the result sets of the quarries'''
+c = conn.cursor()
+# c.execute("""CREATE TABLE ADDRESSES(
+#             first_name text ,
+#             last_name text,
+#             address text,
+#             city text
+#             state text
+#             zipcode integer
+# )
+# """)
+# print("Table created successfully")
 
-#creating cursor
-'''curser is an instance using which you can invoke methords that
-execute SQLITE statement, ferch data from the result sets of the querirs'''
-c= conn.cursor()
-c.execute("""CREATE TABLE ADDRESSES(
-            first_name text,
-            last_name text,
-            address text,
-            city text
-            state text
-            zipcode integer
-)
-""")
-print("Table created successfully")
-
-#commit change
+# commit change
 conn.commit()
 conn.close()
 root.mainloop()
