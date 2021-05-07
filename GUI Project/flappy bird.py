2258b7ef9c04a2aee07d1a9be1a2e7341b388b28
Administrator <nepal3491@gmail.com>
2021-05-07 13:44:03 +0545

    demo

--- GUI Project/flappy bird.py
+++ GUI Project/flappy bird.py
@@ -6,6 +6,7 @@ screen = pygame.display.set_mode((500, 500))
 speed = pygame.time.Clock()
 # adding loop to make pygame work until whe user wants to quit
 while True:
+
     pygame.display.update()
     speed.tick(150)
     for movement in pygame.event.get():
