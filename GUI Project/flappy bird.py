85e8d4455eb0ede897c23bcc24095b7a6824bdb1
Administrator <nepal3491@gmail.com>
2021-05-13 13:24:59 +0545

    commit no 2

--- GUI Project/flappy bird.py
+++ GUI Project/flappy bird.py
@@ -1,10 +1,35 @@
 import pygame  # importing pygame
 import sys  # importing sys
 
+# initialize pygame
 pygame.init()
+
+# creating a screen
 screen = pygame.display.set_mode((500, 500))
+
+# title and icon
+pygame.display.set_caption("FLAPPY BIRD")
+icon = pygame.image.load('redbird-upflap.png')
+pygame.display.set_icon(icon)
+# Adjusting game speed
 speed = pygame.time.Clock()
-# adding loop to make pygame work until whe user wants to quit
+# adding background image
+background = pygame.image.load('background-day.png')
+background = pygame.transform.scale(background, (500, 500))
+# adding flore image
+floor = pygame.image.load('base.png')
+floor = pygame.transform.scale2x(floor)
+floorX = 0
+
+def floor_movement():
+    screen.blit(floor, (floorX, 400))
+    screen.blit(floor, (floorX+500, 400))
+
+
+
+
+
+# game loop
 while True:
 
     pygame.display.update()
@@ -13,3 +38,8 @@ while True:
         if movement.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    screen.blit(background, (0, 0))
+    floorX -= 1
+    floor_movement()
+    if floorX<=-500:
+        floorX=0
--- pygame/pygame 2.py
+++ pygame/pygame 2.py
@@ -2,10 +2,11 @@ import pygame
 import random
 import math
 from pygame import mixer
+
 # initialize pygame
 pygame.init()
 # Create the screen
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
 # Create background
 background = pygame.image.load('MicrosoftTeams-image.png')
 # Background sound
@@ -17,9 +18,11 @@ icon = pygame.image.load('enemy.png')
 pygame.display.set_icon(icon)
 # Adding player
 playerImg = pygame.image.load('space-invaders.png')
+playerImg = pygame.transform.scale(playerImg, (100, 100))
 playerX = 370
 playerY = 480
 playerX_change = 0
+
 # Adding enemy / multiple enemies
 enemyImg = []
 enemyX = []
@@ -30,14 +33,15 @@ num_of_enemies = 5
 
 for i in range(num_of_enemies):
     enemyImg.append(pygame.image.load('enemy.png'))
-    enemyX.append(random.randint(0,735))
-    enemyY.append(random.randint(50,150))
+    enemyX.append(random.randint(0, 735))
+    enemyY.append(random.randint(50, 150))
     # increase the speed based upon your needs
     enemyX_change.append(1)
     enemyY_change.append(40)
 
 # Adding bullet
 bulletImg = pygame.image.load('bullet.png')
+bulletImg = pygame.transform.scale(bulletImg, (50, 50))
 bulletX = 0
 bulletY = 480
 bulletX_change = 0
@@ -48,36 +52,47 @@ bullet_state = "ready"
 
 # score
 score_value = 0
-font = pygame.font.Font('freesansbold.ttf',32)
+font = pygame.font.Font('freesansbold.ttf', 32)
 textX = 10
 textY = 10
 # Game Over Text
-over_font = pygame.font.Font('freesansbold.ttf',68)
+over_font = pygame.font.Font('freesansbold.ttf', 68)
+
 
 # x and y coordinate passed
-def show_score(x,y):
-    score = font.render("Score : " + str(score_value), True, (255,255,255))
-    screen.blit(score,(x,y))
-def game_over_text():
-    over_text = over_font.render(" GAME OVER ", True, (255,255,255))
-    screen.blit(over_text,(200,250))
-def player(x,y):
-    #blit --> draw
-    screen.blit(playerImg, (x,y))
+def show_score(x, y):
+    score = font.render("Score : " + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
 
-def enemy(x,y,i):
-    #blit --> draw
-    screen.blit(enemyImg[i], (x,y))
-def fire_bullet(x,y):
+
+def game_over_text():
+    over_text = over_font.render(" GAME OVER ", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
+
+
+def player(x, y):
+    # blit --> draw
+    screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y, i):
+    # blit --> draw
+    screen.blit(enemyImg[i], (x, y))
+
+
+def fire_bullet(x, y):
     global bullet_state
     bullet_state = "fire"
-    screen.blit(bulletImg,(x+16, y+10))
+    screen.blit(bulletImg, (x + 16, y + 10))
+
+
 def isCollision(enemyX, enemyY, bulletX, bulletY):
-    distance = math.sqrt((math.pow(enemyX-bulletX,2)) + (math.pow(enemyY - bulletY,2)))
+    distance = math.sqrt((math.pow(enemyX - bulletX, 2)) + (math.pow(enemyY - bulletY, 2)))
     if distance < 27:
         return True
     else:
         return False
+
 
 # Game loop
 running = True
@@ -85,7 +100,7 @@ while running:
     # RGB COLOR TO RGB for background color
     screen.fill((0, 110, 178))
     # background image makes speed slow--> needs to increase speed
-    screen.blit(background,(0,0))
+    screen.blit(background, (0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -103,7 +118,7 @@ while running:
                     bullet_Sound.play()
                     # Get the current x coordinate of the spaceship
                     bulletX = playerX
-                    fire_bullet(bulletX,bulletY)
+                    fire_bullet(bulletX, bulletY)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -140,18 +155,16 @@ while running:
             score_value += 1
             enemyX[i] = random.randint(0, 735)
             enemyY[i] = random.randint(50, 150)
-        enemy(enemyX[i], enemyY[i],i)
+        enemy(enemyX[i], enemyY[i], i)
     # Bullet movement
-    if bulletY <=0:
+    if bulletY <= 0:
         bulletY = 480
         bullet_state = "ready"
     if bullet_state is "fire":
         fire_bullet(bulletX, bulletY)
         bulletY -= bulletY_change
 
-
-
     player(playerX, playerY)
-    show_score(textX,textY)
+    show_score(textX, textY)
 
     pygame.display.update()
