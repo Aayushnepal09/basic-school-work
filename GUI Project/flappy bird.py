f37b9f6710d195b7880c454a7d4b97772a701e4e
Administrator <nepal3491@gmail.com>
2021-05-15 14:19:48 +0545

    commit no 4

--- GUI Project/flappy bird.py
+++ GUI Project/flappy bird.py
@@ -31,6 +31,13 @@ bird_box = bird.get_rect(center=(100, 250))
 fall = 0.5  # gravity
 bird_movement = 0
 
+# adding pipes/multipal pipes
+pipe_load = pygame.image.load('pipe-green.png')
+pipe_load = pygame.transform.scale2x(pipe_load)
+pipe_list = []
+pipe_timer = pygame.USEREVENT
+pygame.time.set_timer(pipe_timer, 1000)
+
 
 # function to make floor continuously moving in the screen
 def floor_movement():
@@ -38,6 +45,15 @@ def floor_movement():
     screen.blit(floor, (floorX + 500, 400))
 
 
+def pipe_appear():
+    pipe_new = pipe_load.get_rect(midtop=(200, 250))
+    return pipe_new
+
+
+def pipe_movement(pipes):
+    for pipe in pipes:
+        pipe.center -= 5
+        return pipes
 
 
 # game loop
@@ -51,14 +67,21 @@ while True:
             pygame.quit()
             sys.exit()
 
+        # if key is Spacebar bird will move upward
+        if movement.type == pygame.KEYDOWN:
+            if movement.key == pygame.K_SPACE:
+                bird_movement = 0
+                bird_movement -= 10
+        if movement.type == pipe_timer:
+            pipe_list.append(pipe_appear())
     screen.blit(background, (0, 0))
     # bird movement
     screen.blit(bird, bird_box)
     bird_movement += fall
     bird_box.centery += bird_movement
+
     # floor movement
     floorX -= 1
     floor_movement()
-
     if floorX <= -500:
         floorX = 0
